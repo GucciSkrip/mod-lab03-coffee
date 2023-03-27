b07d1041eaa1e25f2fcdf1e05aea92354a64aec7
GucciSkrip <servolkinc@gmail.com>
2023-03-28 01:44:13 +0300

    vesion 1.4

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,62 +4,62 @@
 #include "Automata.h"
 
 TEST(task1, test1) {
-Automata coffee = Automata();
-coffee.on();
-coffee.getMenu();
-coffee.choice(1);
-coffee.coin(20);
-EXPECT_EQ(Automata::CHECK, coffee.getState());
-EXPECT_FALSE(coffee.check());
-EXPECT_EQ(Automata::WAIT, coffee.getState());
+Automata a = Automata();
+a.on();
+a.getMenu();
+a.choice(1);
+a.coin(20);
+EXPECT_EQ(Automata::CHECK, a.getState());
+EXPECT_FALSE(a.check());
+EXPECT_EQ(Automata::WAIT, a.getState());
 }
 
 TEST(task1, test2) {
-Automata coffee = Automata();
-coffee.on();
-coffee.getMenu();
-coffee.choice(2);
-coffee.coin(50);
-EXPECT_EQ(Automata::CHECK, coffee.getState());
-EXPECT_FALSE(coffee.check());
-EXPECT_EQ(Automata::WAIT, coffee.getState());
+Automata a = Automata();
+a.on();
+a.getMenu();
+a.choice(2);
+a.coin(50);
+EXPECT_EQ(Automata::CHECK, a.getState());
+EXPECT_FALSE(a.check());
+EXPECT_EQ(Automata::WAIT, a.getState());
 }
 
 TEST(task1, test3) {
-Automata coffee = Automata();
-coffee.on();
-coffee.getMenu();
-coffee.choice(3);
-coffee.coin(40);
-coffee.check();
-EXPECT_EQ(Automata::COOK, coffee.getState());
-coffee.finish();
-EXPECT_EQ(Automata::WAIT, coffee.getState());
+Automata a = Automata();
+a.on();
+a.getMenu();
+a.choice(3);
+a.coin(40);
+a.check();
+EXPECT_EQ(Automata::COOK, a.getState());
+a.finish();
+EXPECT_EQ(Automata::WAIT, a.getState());
 }
 
 TEST(task1, test4) {
-Automata coffee = Automata();
-coffee.on();
-coffee.getMenu();
-coffee.choice(1);
-coffee.coin(50);
-coffee.check();
-EXPECT_EQ(Automata::COOK, coffee.getState());
-coffee.finish();
-EXPECT_EQ(0, coffee.getCash());
+Automata a = Automata();
+a.on();
+a.getMenu();
+a.choice(1);
+a.coin(50);
+a.check();
+EXPECT_EQ(Automata::COOK, a.getState());
+a.finish();
+EXPECT_EQ(0, a.getCash());
 }
 
 TEST(task1, test5) {
-Automata coffee = Automata();
-coffee.on();
-coffee.off();
-EXPECT_EQ(Automata::OFF, coffee.getState());
-coffee.on();
-coffee.getMenu();
-coffee.choice(1);
-coffee.coin(50);
-coffee.check();
-EXPECT_EQ(Automata::COOK, coffee.getState());
-coffee.off();
-EXPECT_EQ(Automata::OFF, coffee.getState());
+Automata a = Automata();
+a.on();
+a.off();
+EXPECT_EQ(Automata::OFF, a.getState());
+a.on();
+a.getMenu();
+a.choice(1);
+a.coin(50);
+a.check();
+EXPECT_EQ(Automata::COOK, a.getState());
+a.off();
+EXPECT_EQ(Automata::OFF, a.getState());
 }
